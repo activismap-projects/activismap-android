0a936e1b9bc098c8f567b45ce70c3f440dc08340
Andersson <ander7agar@gmail.com>
2017-03-24 11:16:24 +0100

    cambiada url de api

--- app/src/main/java/com/entropy_factory/activismap/app/Constants.java
+++ app/src/main/java/com/entropy_factory/activismap/app/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
     }
 
     public static final class API {
-        public static final String BASE_URL = APP.DEBUG_MODE ? "https://activismap-api.owldevelopers.com/" : "https://activismap-api.owldevelopers.com/";
+        public static final String BASE_URL = APP.DEBUG_MODE ? "https://activismap-api.owldevelopers.tk/" : "https://activismap-api.owldevelopers.tk/";
         public static final String CLIENT_ID = APP.DEBUG_MODE ? "1_46wnhc8wzoqo0sgkk4cgsgsskw0wos080cwsswkso8co0kg0w4" : "1_46wnhc8wzoqo0sgkk4cgsgsskw0wos080cwsswkso8co0kg0w4";
         public static final String CLIENT_SECRET = APP.DEBUG_MODE ? "7vnu6sdezpk4w0wg0kgkcgg4ocggw40040kscs8ksc4oogko0" : "7vnu6sdezpk4w0wg0kgkcgg4ocggw40040kscs8ksc4oogko0";
     }
